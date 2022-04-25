c5350f6b8b93304c1010d5b57245604b7dd23d1c
SpitzerGodoi <leandro_spitzer@hotmail.com>
2022-04-25 15:34:52 -0300

    ElGeladon

---
+++ src/database/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectToDatabase = () => {
+  mongoose
+    .connect('mongodb://localhost:27017/paletas-db', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('MongoDB Conectado'))
+    .catch((err) =>
+      console.log(`Erro ao conectar com o MONGO-DB, erro: ${err}`),
+    );
+};
+
+module.exports = connectToDatabase;
---
+++ src/middlewares/paleta.middleware.js
@@ -0,0 +1,30 @@
+const { default: mongoose } = require('mongoose');
+
+const validId = (req, res, next) => {
+  const idParam = req.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(idParam)) {
+    return res.status(404).send({ message: 'Paleta não encontrada!' });
+  }
+  next();
+};
+
+const validObjectBody = (req, res, next) => {
+  const paleta = req.body;
+  if (
+    !paleta ||
+    !paleta.sabor ||
+    !paleta.descricao ||
+    !paleta.foto ||
+    !paleta.preco
+  ) {
+    return res.status(400).send({
+      mensagem: 'Você não preencheu todos os campos!',
+    });
+  }
+};
+
+module.exports = {
+  validId,
+  validObjectBody,
+};
