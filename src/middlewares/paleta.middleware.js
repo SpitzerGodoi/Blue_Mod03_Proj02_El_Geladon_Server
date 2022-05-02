66cdecddcb192e0c1fe0737d633bf9ee0e907b87
Leandro Spitzer Godoi <leandro_spitzer@hotmail.com>
2022-05-02 20:29:26 -0300

    ElGeladon

--- src/middlewares/paleta.middleware.js
+++ src/middlewares/paleta.middleware.js
@@ -8,7 +8,6 @@ const validId = (req, res, next) => {
   next();
 };
 
-
 const validObjectBody = (req, res, next) => {
   const paleta = req.body;
   if (
@@ -25,7 +24,21 @@ const validObjectBody = (req, res, next) => {
   next();
 };
 
+const validObjectBodyCarrinho = (req, res, next) => {
+  const carrinho = req.body;
+
+  carrinho.forEach((item) => {
+    if (!item || !item.paletaId || !item.quantidade) {
+      return res.status(400).send({
+        mensagem: 'Você não preencheu todos os campos!',
+      });
+    }
+  });
+  next();
+};
+
 module.exports = {
   validId,
   validObjectBody,
+  validObjectBodyCarrinho,
 };
--- src/routes/paleta.route.js
+++ src/routes/paleta.route.js
@@ -3,6 +3,7 @@ const controllerPaletas = require('../controllers/paleta.controller');
 const {
   validId,
   validObjectBody,
+  validObjectBodyCarrinho,
 } = require('../middlewares/paleta.middleware');
 
 route.get('/all-paletas', controllerPaletas.findAllPaletasController);
@@ -26,6 +27,21 @@ route.delete(
   '/delete-paleta/:id',
   validId,
   controllerPaletas.deletePaletaController,
+);
+
+const controllerCarinho = require('../controllers/carrinho.controller');
+
+route.get('/all-carrinho', controllerCarinho.findAllCarrinhoController);
+
+route.post(
+  '/create-carrinho',
+  validObjectBodyCarrinho,
+  controllerCarinho.createManyItemsCarrinhoController,
+);
+
+route.delete(
+  '/finish-carrinho',
+  controllerCarinho.deleteAllItemsCarrinhoController,
 );
 
 module.exports = route;
