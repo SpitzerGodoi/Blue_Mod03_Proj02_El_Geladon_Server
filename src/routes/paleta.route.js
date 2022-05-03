925a5b448fed2950e9cb5b52780c66d599229fdb
Leandro Spitzer Godoi <leandro_spitzer@hotmail.com>
2022-05-02 22:34:21 -0300

    ElGeladon

--- src/routes/paleta.route.js
+++ src/routes/paleta.route.js
@@ -1,10 +1,16 @@
 const route = require('express').Router();
 const controllerPaletas = require('../controllers/paleta.controller');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
+
 const {
   validId,
   validObjectBody,
   validObjectBodyCarrinho,
 } = require('../middlewares/paleta.middleware');
+
+route.use('/api-docs', swaggerUi.serve);
+route.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 route.get('/all-paletas', controllerPaletas.findAllPaletasController);
 route.get(
@@ -32,13 +38,11 @@ route.delete(
 const controllerCarinho = require('../controllers/carrinho.controller');
 
 route.get('/all-carrinho', controllerCarinho.findAllCarrinhoController);
-
 route.post(
   '/create-carrinho',
   validObjectBodyCarrinho,
   controllerCarinho.createManyItemsCarrinhoController,
 );
-
 route.delete(
   '/finish-carrinho',
   controllerCarinho.deleteAllItemsCarrinhoController,
